c09bade504bdb9669aa19e9c2fabb836b06b3b37
safouene <safouene-abbachi@hotmail.fr>
2023-04-12 12:23:50 +0100

    DEV : Activating account + login backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,7 @@ const { generateToken } = require('../helpers/tokens');
 const { validateUsername } = require('../helpers/validation');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try {
@@ -51,8 +52,52 @@ exports.register = async (req, res) => {
       verified: user.verified,
       message: 'Registration success! Please activate your account to start',
     });
-    res.json(user);
   } catch (error) {
     res.json(error.message);
+  }
+};
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const checked = await User.findById(user.id);
+    if (checked.verified) {
+      return res.status(400).json({ message: 'Account already activated !' });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res
+        .status(200)
+        .json({ message: 'Account successfully activated !' });
+    }
+  } catch (error) {
+    return res.json({ error: error.message });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: 'The email you entered is not linked to any account !',
+      });
+    }
+    const decryptPassword = await bcrypt.compare(password, user.password);
+    if (!decryptPassword) {
+      return res.status(400).json({ message: 'Invalid credentials !' });
+    }
+    const token = generateToken({ id: user._id.toString() }, '7d');
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: 'Registration success! Please activate your account to start',
+    });
+  } catch (error) {
+    res.json({ error: error.message });
   }
 };
